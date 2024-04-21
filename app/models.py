bd97789d856c3383fb9fefe5149d8dc66c9b30bd
Zeezbaba <Azeez_okeleji>
2024-04-21 17:52:53 +0100

    User Authentication

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from hashlib import md5
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
@@ -22,6 +24,14 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
 @login.user_loader
 def load_user(id):
