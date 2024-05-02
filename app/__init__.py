aa9d3d3696293388d5b05d99e8923c30f45423a7
Adejare <adejare.arasheed@gmail.com>
2024-05-02 12:02:29 +0100

    rash_zeez

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+Session(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
