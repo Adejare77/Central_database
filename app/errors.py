40893d5b169ad104406412f16c76c1747fe74988
Adejare <adejare.arasheed@gmail.com>
2024-05-15 09:30:46 +0100

    Routes Error Handlers

--- app/errors.py
+++ app/errors.py
@@ -2,10 +2,12 @@
 from flask import render_template
 from app import app, db
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     """Handles error 404"""
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
