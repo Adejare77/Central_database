e73036202da98f8201696b69402bac4917c00a67
Adejare <adejare.arasheed@gmail.com>
2024-05-15 09:31:58 +0100

    Add or Delete users from Central_DB

--- app/user.py
+++ app/user.py
@@ -8,27 +8,22 @@ from app.database import Database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
-import subprocess
 from app.sqldump import DumpCleanUp
-from os import getenv
 from datetime import date
+
 
 class MyUser():
     """Represents a user and provides methods for adding and
     deleting users, checking folders, and managing database connections.
     """
-    acceptable_format = {
-        "mysql+mysqldb": ['.frm', '.ibd', '.myd', '.myi', '.ibdata'],
-        "postgresql": ['.pgsql', '.pgdata', '.sql', '.dump'],
-        "sqlite": ['.sqlite', '.db', '.sqlite3'],
-        "microsoft": ['.mdf', '.ldf', '.bak', '.ndf'],
-        "oracle": ['.log', '.dbf', '.ctl'],
-        "mariadb": ['.ibd', '.frm']
-        }
-    server_path = {
-        "mysql+mysqldb": "/var/lib/mysql/",
-    }
+
     def __init__(self, id, username) -> None:
+        """Initializes the user's id and username
+
+        Args:
+            id (int): The unique user's id
+            username (_type_): The unique user's username
+        """
         url = 'mysql+mysqldb://{}:{}@{}:3306/{}'.format(\
             'my_user', 'my_user_passwd', 'localhost', 'central_db')
         self.id = id
@@ -37,6 +32,7 @@ class MyUser():
         self.Session = sessionmaker(bind=self.engine)
 
     def addUser(self):
+        """adds a new user to central_db"""
         session = self.Session()
         new_user = UserDatabase(id=self.id, username=self.username)
         session.add(new_user)
@@ -44,20 +40,19 @@ class MyUser():
         session.close()
 
     def delUser(self):
+        """deletes a user from the central_db"""
         session = self.Session()
         query = session.query(UserDatabase).filter_by(id=self.id).delete()
         session.commit()
         session.close()
 
     def check_folder(self, uploaded_files, filename, db_engine=None):
+        """ checks if the sqldump file will be succesfully uploaded"""
         try:
-            # filename = uploaded_files.filename
             home = os.path.expanduser("~/Desktop")
             central_db_path = os.path.join(home, "central_db", self.username)
             os.makedirs(central_db_path, exist_ok=True)
-            # path = os.path.join(central_db_dir, filename)
             db_name = self.username + "_" + filename
-            # uploaded_files.save(path)
             uploaded_files.save(os.path.join(central_db_path, filename))
             returned_fmt = DumpCleanUp(filename, db_name, central_db_path).dump_data(db_engine)
             if not returned_fmt:
@@ -69,4 +64,5 @@ class MyUser():
             return False
 
     def __del__(self):
+        """Dispose the central_db engine"""
         self.engine.dispose()
